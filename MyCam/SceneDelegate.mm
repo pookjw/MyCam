ce570f2be0024fd7ad0fdc3a2181242a4155cb63
Jinwoo Kim <kidjinwoo@me.com>
2024-09-17 21:23:17 +0900

    no message

--- MyCam/SceneDelegate.mm
+++ MyCam/SceneDelegate.mm
@@ -8,9 +8,21 @@
 #import "SceneDelegate.h"
 #import "CameraRootViewController.h"
 #import <AVFoundation/AVFoundation.h>
+#import <Photos/Photos.h>
+#import <CoreLocation/CoreLocation.h>
 #import <CamPresentation/CameraRootViewController.h>
+#import <objc/runtime.h>
+
+@interface SceneDelegate () <CLLocationManagerDelegate>
+@property (class, nonatomic, readonly) void *didChangeAuthorizationKey;
+@end
 
 @implementation SceneDelegate
+
++ (void *)didChangeAuthorizationKey {
+    static void *key = &key;
+    return key;
+}
 
 - (void)dealloc {
     [_window release];
@@ -27,24 +39,17 @@
         [cameraRootViewController restoreStateWithUserActivity:stateRestorationActivity];
     }
     
-    switch ([AVCaptureDevice authorizationStatusForMediaType:AVMediaTypeVideo]) {
-        case AVAuthorizationStatusAuthorized: {
-            [navigationController setViewControllers:@[cameraRootViewController] animated:NO];
-            break;
-        }
-        case AVAuthorizationStatusNotDetermined: {
-            [AVCaptureDevice requestAccessForMediaType:AVMediaTypeVideo completionHandler:^(BOOL granted) {
-                assert(granted);
-                
-                dispatch_async(dispatch_get_main_queue(), ^{
-                    [navigationController setViewControllers:@[cameraRootViewController] animated:NO];
-                });
-            }];
-            break;
-        }
-        default:
-            abort();
-    }
+    [self requestAuthorizationsWithCompletionHandler:^(BOOL granted) {
+        dispatch_async(dispatch_get_main_queue(), ^{
+            if (granted) {
+                [navigationController setViewControllers:@[cameraRootViewController] animated:NO];
+            } else {
+                [scene openURL:[NSURL URLWithString:UIApplicationOpenSettingsURLString] options:nil completionHandler:^(BOOL success) {
+                    exit(EXIT_FAILURE);
+                }];
+            }
+        });
+    }];
     [cameraRootViewController release];
     
     //
@@ -84,6 +89,105 @@
     if (![cameraRootViewController isKindOfClass:CameraRootViewController.class]) return nil;
     
     return cameraRootViewController.stateRestorationActivity;
+}
+
+- (void)requestAuthorizationsWithCompletionHandler:(void (^ _Nullable)(BOOL granted))completionHandler {
+    void (^requestLocationAuthorization)() = ^{
+        CLLocationManager *locationManager = [CLLocationManager new];
+        
+        switch (locationManager.authorizationStatus) {
+            case kCLAuthorizationStatusNotDetermined:
+                locationManager.delegate = self;
+                
+                objc_setAssociatedObject(locationManager,
+                                         SceneDelegate.didChangeAuthorizationKey,
+                                         ^(CLLocationManager *locationManager) {
+                    switch (locationManager.authorizationStatus) {
+                        case kCLAuthorizationStatusAuthorizedAlways:
+                        case kCLAuthorizationStatusAuthorizedWhenInUse:
+                            completionHandler(YES);
+                            break;
+                        default:
+//                            completionHandler(NO);
+                            break;
+                    }
+                },
+                                         OBJC_ASSOCIATION_COPY_NONATOMIC);
+                
+                [locationManager requestWhenInUseAuthorization];
+                break;
+            case kCLAuthorizationStatusAuthorizedAlways:
+            case kCLAuthorizationStatusAuthorizedWhenInUse:
+                completionHandler(YES);
+                break;
+            default:
+                completionHandler(NO);
+                break;
+        }
+        
+        [locationManager release];
+    };
+    
+    void (^requestPhotoLibraryAuthorization)() = ^{
+        switch ([PHPhotoLibrary authorizationStatusForAccessLevel:PHAccessLevelReadWrite]) {
+            case PHAuthorizationStatusNotDetermined:
+                [PHPhotoLibrary requestAuthorizationForAccessLevel:PHAccessLevelReadWrite handler:^(PHAuthorizationStatus status) {
+                    switch (status) {
+                        case PHAuthorizationStatusAuthorized:
+                        case PHAuthorizationStatusLimited:
+                            requestLocationAuthorization();
+                            break;
+                        default:
+                            completionHandler(NO);
+                            break;
+                    }
+                }];
+                break;
+            case PHAuthorizationStatusAuthorized:
+            case PHAuthorizationStatusLimited:
+                requestLocationAuthorization();
+                break;
+            default:
+                completionHandler(NO);
+                break;
+        }
+        
+       
+    };
+    
+    void (^requestCameraAuthorization)() = ^{
+        switch ([AVCaptureDevice authorizationStatusForMediaType:AVMediaTypeVideo]) {
+            case AVAuthorizationStatusAuthorized: {
+                requestPhotoLibraryAuthorization();
+                break;
+            }
+            case AVAuthorizationStatusNotDetermined: {
+                [AVCaptureDevice requestAccessForMediaType:AVMediaTypeVideo completionHandler:^(BOOL granted) {
+                    if (!granted) {
+                        completionHandler(NO);
+                        return;
+                    }
+                    
+                    requestPhotoLibraryAuthorization();
+                }];
+                break;
+            }
+            default:
+                completionHandler(NO);
+                break;
+        }
+    };
+    
+    requestCameraAuthorization();
+}
+
+- (void)locationManagerDidChangeAuthorization:(CLLocationManager *)manager {
+    auto block = (void (^ _Nullable)(id))(objc_getAssociatedObject(manager, SceneDelegate.didChangeAuthorizationKey));
+    
+    if (block) {
+        block(manager);
+//        objc_setAssociatedObject(manager, SceneDelegate.didChangeAuthorizationKey, nil, OBJC_ASSOCIATION_ASSIGN);
+    }
 }
 
 @end
