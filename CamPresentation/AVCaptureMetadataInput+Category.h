4395f426f59cc5d4c8a8844f1e1560d07eb12d05
Jinwoo Kim <kidjinwoo@me.com>
2025-07-08 00:28:59 +0900

    no message

---
+++ CamPresentation/AVCaptureMetadataInput+Category.h
@@ -0,0 +1,16 @@
+//
+//  AVCaptureMetadataInput+Category.h
+//  CamPresentation
+//
+//  Created by Jinwoo Kim on 7/8/25.
+//
+
+#import <AVFoundation/AVFoundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface AVCaptureMetadataInput (Category)
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ CamPresentation/AVCaptureMetadataInput+Category.m
@@ -0,0 +1,17 @@
+//
+//  AVCaptureMetadataInput+Category.m
+//  CamPresentation
+//
+//  Created by Jinwoo Kim on 7/8/25.
+//
+
+#import "AVCaptureMetadataInput+Category.h"
+
+@implementation AVCaptureMetadataInput (Category)
+
+// https://github.com/pookjw/AppleFeedback/tree/main/FB18666885
+- (AVCaptureDevice *)device {
+    return nil;
+}
+
+@end
